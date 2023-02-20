4a5920cb53f543ebf73fe0f949afb0030fdc3835
jesscarlett <canrrytan@hotmail.com>
2023-02-20 20:55:06 +1100

    2 commit, more html pages and functions.

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path('dictionary', views.dict_view, name='dict_view'),
     path('pswords', views.junior_senior_words, name='pswords'),
     path('seniorwords', views.senior_words, name='seniorwords'),
-    path('jokes_quotes', views.jokes_quotes, name='jokes_quotes')
+    path('jokes_quotes', views.jokes_quotes, name='jokes_quotes'),
+    path('quotes', views.quotes, name='quotes'),
+    path('conversations', views.conversations, name='conversations'),
+    path('choice_helper', views.choice_helper, name='choice_helper'),
+    path('thesaurus', views.thesaurus, name='thesaurus'),
+    path('fill_sentence', views.fill_sentence, name='fill_sentence'),
+    path('paragraph', views.paragraph, name='paragraph')
 ]
 

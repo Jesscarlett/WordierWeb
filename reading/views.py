fc863737c06ed8eefdf5b69d7e5bea4708294086
jesscarlett <canrrytan@hotmail.com>
2023-03-25 19:28:56 +1100

    15 commit, reading comprehension

--- reading/views.py
+++ reading/views.py
@@ -23,3 +23,61 @@ def alice_three(request):
 
 def alice_four(request):
     return render(request, 'alice_four.html')
+
+def alice_five(request):
+    return render(request, 'alice_five.html')
+
+
+def alice_six(request):
+    return render(request, 'alice_six.html')
+
+def alice_seven(request):
+    return render(request, 'alice_seven.html')
+
+def alice_eight(request):
+    return render(request, 'alice_eight.html')
+
+def alice_nine(request):
+    return render(request, 'alice_nine.html')
+
+def alice_ten(request):
+    return render(request, 'alice_ten.html')
+
+def alice_eleven(request):
+    return render(request, 'alice_eleven.html')
+
+def alice_twelve(request):
+    return render(request, 'alice_twelve.html')
+
+def glass_one(request):
+    return render(request, 'glass_one.html')
+
+def glass_two(request):
+    return render(request, 'glass_two.html')
+
+def glass_three(request):
+    return render(request, 'glass_three.html')
+
+def glass_four(request):
+    return render(request, 'glass_four.html')
+
+def glass_five(request):
+    return render(request, 'glass_five.html')
+
+def glass_six(request):
+    return render(request, 'glass_six.html')
+
+def glass_seven(request):
+    return render(request, 'glass_seven.html')
+
+def glass_eight(request):
+    return render(request, 'glass_eight.html')
+
+def glass_nine(request):
+    return render(request, 'glass_nine.html')
+
+def glass_ten(request):
+    return render(request, 'glass_ten.html')
+
+def glass_eleven(request):
+    return render(request, 'glass_eleven.html')

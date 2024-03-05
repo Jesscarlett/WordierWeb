4cde0ba4d5b7b5c9593b91f2146283c13f2d669b
jesscarlett <canrrytan@hotmail.com>
2024-03-05 21:57:47 +1100

    33th commit,reading classics 3

--- reading/views.py
+++ reading/views.py
@@ -117,3 +117,57 @@ def peter_five(request):
 
 def peter_six(request):
     return render(request, 'peter_six.html')
+
+def black_one(request):
+    return render(request, 'black_one.html')
+
+def black_two(request):
+    return render(request, 'black_two.html')
+
+def black_three(request):
+    return render(request, 'black_three.html')
+
+def black_four(request):
+    return render(request, 'black_four.html')
+
+def black_five(request):
+    return render(request, 'black_five.html')
+
+def black_six(request):
+    return render(request, 'black_six.html')
+
+def black_seven(request):
+    return render(request, 'black_seven.html')
+
+def black_eight(request):
+    return render(request, 'black_eight.html')
+
+def black_nine(request):
+    return render(request, 'black_nine.html')
+
+def black_ten(request):
+    return render(request, 'black_ten.html')
+
+def fairy_one(request):
+    return render(request, 'fairy_one.html')
+
+def fairy_two(request):
+    return render(request, 'fairy_two.html')
+
+def fairy_three(request):
+    return render(request, 'fairy_three.html')
+
+def fairy_four(request):
+    return render(request, 'fairy_four.html')
+
+def fairy_five(request):
+    return render(request, 'fairy_five.html')
+
+def fairy_six(request):
+    return render(request, 'fairy_six.html')
+
+def fairy_seven(request):
+    return render(request, 'fairy_seven.html')
+
+def fairy_eight(request):
+    return render(request, 'fairy_eight.html')

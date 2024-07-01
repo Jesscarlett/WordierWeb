07da2bdcc3984dbb1ba1a06b11fa03f8383bd54f
jesscarlett <canrrytan@hotmail.com>
2024-07-01 23:13:36 +1000

    49th commit AI - sitemap

--- dictionary/sitemaps.py
+++ dictionary/sitemaps.py
@@ -7,7 +7,7 @@ class StaticViewSitemap(Sitemap):
     priority = 0.8
 
     def items(self):
-        return ['home', 'about', 'match', 'read', 'fivehundredwords', 'fivehundredgame', 'sixhundredwords', 'sixhundredgame',
+        return ['home', 'about', 'chat', 'hchat', 'match', 'read', 'fivehundredwords', 'fivehundredgame', 'sixhundredwords', 'sixhundredgame',
                 'sevenhundredwords', 'sevenhundredgame', 'onethousandadj', 'aboutai', 'pswords', 'seniorwords', 'quotes', 'conversations',
                 'choice_helper', 'thesaurus', 'fill_sentence', 'paragraph', 'hangman_junior', 'privacy']
 

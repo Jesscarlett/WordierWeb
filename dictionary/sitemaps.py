3d938bfc1a2985ca2feb6acd2786a9401dca3774
jesscarlett <canrrytan@hotmail.com>
2025-01-01 23:00:32 +1100

    65 commit books added audio with fixes

--- dictionary/sitemaps.py
+++ dictionary/sitemaps.py
@@ -7,7 +7,7 @@ class StaticViewSitemap(Sitemap):
     priority = 0.8
 
     def items(self):
-        return ['home', 'about', 'chat', 'hchat', 'match', 'read', 'book', 'fivehundredwords', 'fivehundredgame', 'sixhundredwords', 'sixhundredgame',
+        return ['home', 'about', 'chat', 'hchat', 'match', 'read', 'book', 'alpha', 'firstwords', 'fivehundredwords', 'fivehundredgame', 'sixhundredwords', 'sixhundredgame',
                 'sevenhundredwords', 'sevenhundredgame', 'onethousandadj', 'aboutai', 'pswords', 'seniorwords', 'quotes', 'conversations',
                 'choice_helper', 'thesaurus', 'fill_sentence', 'paragraph', 'hangman_junior', 'privacy']
 

b1c358a8c03233d4f238058567026232b170f214
jesscarlett <canrrytan@hotmail.com>
2025-01-01 22:58:04 +1100

    64 commit books added audio with fixes

--- dictionary/sitemaps.py
+++ dictionary/sitemaps.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 
 
 class StaticViewSitemap(Sitemap):
-    changefreq = "weekly"
+    changefreq = "daily"
     priority = 0.8
 
     def items(self):
-        return ['home', 'about', 'chat', 'hchat', 'match', 'read', 'fivehundredwords', 'fivehundredgame', 'sixhundredwords', 'sixhundredgame',
+        return ['home', 'about', 'chat', 'hchat', 'match', 'read', 'book', 'fivehundredwords', 'fivehundredgame', 'sixhundredwords', 'sixhundredgame',
                 'sevenhundredwords', 'sevenhundredgame', 'onethousandadj', 'aboutai', 'pswords', 'seniorwords', 'quotes', 'conversations',
                 'choice_helper', 'thesaurus', 'fill_sentence', 'paragraph', 'hangman_junior', 'privacy']
 
